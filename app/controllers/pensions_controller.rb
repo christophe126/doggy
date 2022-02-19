d036d81667086547b33d400c6b69faee4f5d413e
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-02-19 09:19:31 +0100

    pundit

--- app/controllers/pensions_controller.rb
+++ app/controllers/pensions_controller.rb
@@ -1,3 +1,7 @@
 class PensionsController < ApplicationController
+  authorize @pension
 
+  def index
+    @pensions = policy_scope(Pension).order(created_at: :desc)
+  end
 end
---
+++ app/policies/pension_policy.rb
@@ -0,0 +1,30 @@
+class PensionPolicy < ApplicationPolicy
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
+  end
+  
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+
+    def resolve
+      scope.all
+
+      # For a multi-tenant SaaS app, you may want to use:
+      # scope.where(user: user)
+    end
+  end
+end
