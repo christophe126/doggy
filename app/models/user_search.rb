3a2125eaa105d0dac3ae427e682eddf69fab8a03
medericmesle <mederic.mesle@gmail.com>
2022-02-19 13:01:05 +0100

    JSON JSON ça c'est fort en JSON

--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -1,7 +1,6 @@
 class UserSearch < ApplicationRecord
-
   # geocoded_by :start_address
   # after_validation :geocode, if: :will_save_change_to_address?
 
-  belongs_to :user_search
+  belongs_to :user
 end
