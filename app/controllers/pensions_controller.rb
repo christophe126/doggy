21b699baa3639e7f48a390e2d0d799bee555e84f
christophe126 <christophe126@gmail.com>
2022-02-23 19:18:56 +0100

    direction2

--- app/controllers/pensions_controller.rb
+++ app/controllers/pensions_controller.rb
@@ -7,12 +7,11 @@ class PensionsController < ApplicationController
     # @pension = Pension.all
     @user_search = UserSearch.where("user_id = #{current_user.id}").last
     @start_marker = [{
-      lng: @user_search.start_lat,
-      lat: @user_search.start_lng
-
+      lng: @user_search.start_lng,
+      lat: @user_search.start_lat
     }, {
-      lng: @user_search.end_lat,
-      lat: @user_search.end_lng
+      lng: @user_search.end_lng,
+      lat: @user_search.end_lat
     }]
     @geoson = @user_search.direction
 
@@ -21,18 +20,23 @@ class PensionsController < ApplicationController
     # recherche des pensions
     @result_array = []
     @res = recup_poi(@search_waypoints)
-    @result = @res.uniq!
-    #-------Envoi des markers-----------#
+    if @res.count.zero?
+      @poi = {}
+    else
+      @result = @res.uniq!
+      #-------Envoi des markers waypoints-----------#
+      @poi = @result.map do |result|
+        {
+          lat: result.latitude,
+          lng: result.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { result: result })
+        }
+      end
 
-    @poi = @result.map do |result|
-      {
-        lat: result.latitude,
-        lng: result.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { result: result })
-      }
     end
-    #raise
     # ------------------------------------------------------------------------
+
+    @new_search = UserSearch.new
   end
 
   private
@@ -41,7 +45,6 @@ class PensionsController < ApplicationController
     waypoints.each do |waypoint|
       @search_records = Pension.near([waypoint[1], waypoint[0]], 50, units: :km)
       @search_records.each do |record|
-        #@result_array << [record.id, record.name, record.longitude, record.latitude]
         @result_array << record
       end
     end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,46 +1,49 @@
 import mapboxgl from 'mapbox-gl';
 
-
+// fonction Ajouter markers départ et arrivée
 const addStartMarker = (map, startMarker) => {
-  console.log(startMarker);
   startMarker.forEach((stMarker) => {
-    new mapboxgl.Marker()
+    new mapboxgl.Marker({ "color": "#FBC42B"})
     .setLngLat([stMarker.lng, stMarker.lat])
     .addTo(map);
   });
 };
 
 
-
+// fonction Ajouter pensions trouvées
 const addPoiToMap = (map, poiPensions) => {
   poiPensions.forEach((poiPension) => {
     const popup = new mapboxgl.Popup().setHTML(poiPension.info_window);
 
-    new mapboxgl.Marker()
+    new mapboxgl.Marker({ "color": "#000000" })
       .setLngLat([poiPension.lng, poiPension.lat])
       .setPopup(popup)
       .addTo(map);
   });
 };
 
+//initialisation et construction de la map
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
   if (mapElement) {
-    // import start point coordinates
+
+    // Appel de la fonction Ajouter markers départ et arrivée
+    const startMarker = JSON.parse(mapElement.dataset.marker);
+
 
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10',
-      center: [2.3522219, 48.856614],
-      zoom: 12
+      center: [startMarker[0].lng, startMarker[0].lat],
+      zoom: 10
     });
 
-    const startMarker = JSON.parse(mapElement.dataset.marker);
-    // for each addStartMarker call the function
+    // Appel de la fonction Ajouter markers départ et arrivée
     addStartMarker(map, startMarker);
 
+    // Construction de la map
     map.on('load', function() {
       const route = JSON.parse(mapElement.dataset.route)
       const geojson = {
@@ -63,16 +66,19 @@ const initMapbox = () => {
           'line-cap': 'round'
         },
         paint: {
-          'line-color': '#3887be',
+          'line-color': '#FBC42B',
           'line-width': 5,
           'line-opacity': 0.75
         }
       });
     })
-    //add poi on may
+    // Appel de la fonction Ajouter pensions trouvées
     const poiPensions = JSON.parse(mapElement.dataset.pensionway);
-    global.map = map;
-    addPoiToMap(map, poiPensions);
+    if (Object.keys(poiPensions).length != 0 ){
+      global.map = map;
+      addPoiToMap(map, poiPensions);
+    }
+
   };
 }
 
