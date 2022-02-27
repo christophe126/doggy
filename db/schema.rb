ead54fa77dc7ee1bee5bf82ec1277fde47090d20
christophe126 <christophe126@gmail.com>
2022-02-27 16:27:25 +0100

    fix show and hide form

--- db/schema.rb
+++ db/schema.rb
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2022_02_25_204735) do
     t.float "latitude"
     t.float "longitude"
     t.integer "rating", default: 0, null: false
+    t.integer "from_price", default: 31, null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
