eb46ea44e23c36dcb3750bf6e6f552e070f5a662
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-02-27 14:32:13 +0100

    view new booking Ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,39 @@
 class BookingsController < ApplicationController
+  skip_after_action :verify_authorized
 
   def index
+    @pensions = policy_scope(Pension)
     @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new # (booking_params)
+    @pension = Pension.find(params[:pension_id])
+    @user_pets = UserPet.where(user: current_user)
+    # @user_search = UserSearch.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    start_date = Date.parse(params[:booking][:start_date])
+    end_date = Date.parse(params[:booking][:end_date])
+    total_days = (end_date - start_date) + 1
+    price_per_day = @pension.price_per_day
+    total_price = price_per_day * total_days
+    total_price = total_price.to_i
+    @booking.total_price = total_price
+    @booking.user = current_user
+    @booking.pension = @pension
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
   end
 end
