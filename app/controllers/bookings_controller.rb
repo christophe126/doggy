4a21fb05d6d3825b49a54234cd1188198bf9f591
mederic mesle <mederic.mesle@gmail.com>
2022-03-03 21:24:10 +0100

    First draft

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,20 @@ class BookingsController < ApplicationController
 
   def index
     @pensions = policy_scope(Pension)
-    @bookings = Booking.where(user_id: current_user)
+    @booking = Booking.all
+    @pension = Pension.all
+    @user_pets = UserPet.where(user: current_user)
+    @user_search = UserSearch.where(user_id: @current_user).last
+    @nb_jours = Nbjour.new(@user_search.start_date, @user_search.end_date)
+    @total_days = @nb_jours.cal_nb_jours
+
+    # @res = []
+    # @sub_total = []
+    # @user_pets.each do |pet|
+    #   @res << [pet.name, PensionPet.select(:price_per_day).where(pet_id: pet.pet_id, pension_id: @pension.id)]
+    #   @sub_total << @res.first[1].first.price_per_day
+    # end
+    # @grant_total = @sub_total.sum * @total_days
   end
 
   def new
@@ -35,7 +48,7 @@ class BookingsController < ApplicationController
   end
 
   def edit
-    @pension =Pension.find(params[:pension_id])
+    @pension = Pension.find(params[:pension_id])
     @booking = Booking.find(params[:id])
   end
 
