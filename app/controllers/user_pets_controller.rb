fccb776ca46bd925bcf073d8172443fd56e773b4
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-03-04 18:11:14 +0100

    user_pets view

--- app/controllers/user_pets_controller.rb
+++ app/controllers/user_pets_controller.rb
@@ -1,6 +1,9 @@
 class UserPetsController < ApplicationController
   def index
     @pensions = policy_scope(Pension)
-    @user_pets = UserPet.all
+    @user = User.where(user: current_user)
+    @user_basket = UserPet.select(:user_basket_id).where(user: current_user).last
+    @user_pets = UserPet.where(user_basket_id: @user_basket.user_basket_id)
   end
+
 end
