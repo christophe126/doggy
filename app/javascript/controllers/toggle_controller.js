a75130da4cdd525e8c59cf49e739b52f6d1e5302
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-03-10 23:24:08 +0100

    Resa Ok - swipe à faire

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,16 +1,21 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "btn", "cards", "swipeList" ]
+  static targets = [ "btn", "cards","swipeListFuture", "swipeListPast" ]
 
   switchView(e) {
+
     if (!e.currentTarget.classList.contains("active")) {
       this.btnTargets.forEach((btn) => {
         btn.classList.toggle("active");
       });
+    
       this.cardsTargets.forEach((cards) => {
         cards.classList.toggle("hidden");
       });
-    }
+      
+    // this.swipeListPastTarget.classList.toggle('swiped-list-right');
+    // this.swipeListFutureTarget.classList.toggle('swiped-list-left');
+   } 
   }
-}
+};
