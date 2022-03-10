db0991c7c5907a371240da022a9e93e5b4e18ba4
christophe126 <christophe126@gmail.com>
2022-03-10 19:37:01 +0100

    fix_errors

--- app/controllers/user_pets_controller.rb
+++ app/controllers/user_pets_controller.rb
@@ -1,8 +1,10 @@
 class UserPetsController < ApplicationController
   def index
-    @pensions = policy_scope(Pension)
+
     @current_user = current_user
     @user_basket = UserPet.select(:user_basket_id).where(user: current_user).last
     @user_pets = UserPet.where(user_basket_id: @user_basket.user_basket_id)
+    @pensions = policy_scope(Pension)
+    raise
   end
 end
