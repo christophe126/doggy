8cba8e71ca6bae0c4df26dce67445371e37c591f
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-03-17 18:46:23 +0100

    x

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,11 +1,11 @@
 import flatpickr from "flatpickr";
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
-  flatpickr("#range_start", {
-    altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end"})]
+
+  console.log("fghjkl");
+  flatpickr(".datepicker", {
+    allowInput: true
   });
-}
+};
 
 export { initFlatpickr };
